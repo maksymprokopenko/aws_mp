2dff9997c5532e36653d865de5b1d27ed4c94262
Maksym Prokopenko <Maksym_Prokopenko@epam.com>
2020-11-08 20:00:42 +0200

    complete task4 FE part

--- fe/src/constants/apiPaths.ts
+++ fe/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
   product: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  order: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
