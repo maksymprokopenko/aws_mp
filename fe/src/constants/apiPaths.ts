38e42b09e886760022afbf425b1df3cca40f5c95
Maksym Prokopenko <Maksym_Prokopenko@epam.com>
2020-11-15 18:59:01 +0200

    connect FE with BE

--- fe/src/constants/apiPaths.ts
+++ fe/src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://hjdievx3dg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://zagkv99l30.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
